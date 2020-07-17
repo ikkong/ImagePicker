f30ade92b9e0c2cf2c42b45bb280d9e6cec96121
ikkong.maipu <ikkong@qq.com>
2020-07-17 15:15:56 +0800

    修复无法选到最新一张照片的bug

--- imagepicker/src/main/java/com/lzy/imagepicker/ImageDataSource.java
+++ imagepicker/src/main/java/com/lzy/imagepicker/ImageDataSource.java
@@ -79,7 +79,6 @@ public class ImageDataSource implements LoaderManager.LoaderCallbacks<Cursor> {
         imageFolders.clear();
         if (data != null) {
             ArrayList<ImageItem> allImages = new ArrayList<>();   //所有图片的集合,不分文件夹
-            data.moveToFirst();
             while (data.moveToNext()) {
                 //查询数据
                 String imageName = data.getString(data.getColumnIndexOrThrow(IMAGE_PROJECTION[0]));
